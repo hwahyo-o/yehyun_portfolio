0cf0c78ca7475e13e577c52d202ea2b48ca2a518
hwahyo-o <ajas03974@gmail.com>
2026-08-12 13:33:58 +0900

    config: add Firebase web authentication settings

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 window.PORTFOLIO_CONFIG = Object.freeze({
   apiBase: 'https://yehyun-portfolio-api.ajas03974.workers.dev',
   firebase: {
+    apiKey: 'AIzaSyDEwL8IlS7zmmMVJBX-tzhbVSMWlDPBkIg',
+    authDomain: 'yehyun-portfolio.firebaseapp.com',
     projectId: 'yehyun-portfolio',
+    appId: '1:968767439402:web:85d38695966d6a5e32f953',
+    messagingSenderId: '968767439402',
   },
 });
